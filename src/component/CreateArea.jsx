3effc3b229260849d4fe6001f82704fbcbd7d805
Phoem Phanith <phanith1826@gmail.com>
2021-02-26 18:50:55 +0700

    Styling App

--- src/component/CreateArea.jsx
+++ src/component/CreateArea.jsx
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
+import Zoom from "@material-ui/core/Zoom";
+import Fab from "@material-ui/core/Fab";
+import AddIcon from "@material-ui/icons/Add";
+// import { Add } from "@material-ui/icons";
 
 function CreateArea(props) {
   const [inputNote, setInputNote] = useState({
     title: "",
     content: "",
   });
+  const [isInput, setIsInput] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -13,24 +18,35 @@ function CreateArea(props) {
     });
   }
 
+  function handleClick() {
+    setIsInput(true);
+  }
+
   return (
     <div>
       <form
+        className="create-note"
         onSubmit={(event) => {
           props.onAdd(inputNote);
           setInputNote({ title: "", content: "" });
+          setIsInput(false);
           event.preventDefault();
         }}
       >
-        <input onChange={handleChange} name="title" placeholder="Title" value={inputNote.title} />
+        {isInput ? <input onChange={handleChange} name="title" placeholder="Title" value={inputNote.title} /> : null}
         <textarea
+          onClick={handleClick}
           onChange={handleChange}
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isInput ? 3 : 1}
           value={inputNote.content}
         />
-        <button type="submit">Add</button>
+        <Zoom in={isInput}>
+          <Fab type="submit">
+            <AddIcon />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import BookIcon from "@material-ui/icons/Book";
 
 function Header() {
   return (
     <header>
-      <h1>Keeper App</h1>
+      <h1>
+        <BookIcon fontSize="large" />
+        Keeper App
+      </h1>
     </header>
   );
 }
